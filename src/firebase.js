f4fda3a5bed8f58fe3b31886a78dc9c0792aca5d
Jenna LeFort <71297454+jenna-vec@users.noreply.github.com>
2022-02-03 18:35:54 -0500

    env keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAP6NuBaDyzPr2aTGdOq34H6qDEwWBvXdE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
