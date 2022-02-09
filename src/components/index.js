43e8458d81745a1501b18ae2ec9d2509a9e80dcc
Jenna LeFort <71297454+jenna-vec@users.noreply.github.com>
2022-02-08 19:59:37 -0500

    Delete src/components directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Navigation } from './Navigation/Navigation';
-export { default as Home } from './Home/Home';
-export { default as Login } from './Login/Login'
-export { default as Register } from './Register/Register';
-export { default as Account } from './Account/Account'
-export { default as Recipe } from './Recipe/Recipe'
